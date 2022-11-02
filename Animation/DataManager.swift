8e2c4931dfed1333ea7560b68cd0cfe8e4211d12
techniquete666 <dodyaozxc@gmail.com>
2022-11-02 18:05:13 +0300

    setup

---
+++ Animation/DataManager.swift
@@ -0,0 +1,73 @@
+//
+//  DataManager.swift
+//  Animation
+//
+//  Created by Vyacheslav on 02.11.2022.
+//
+
+import Foundation
+
+class DataManager {
+    
+    let presets = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+    ]
+    
+    let curves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]    
+}
--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var randomAnimationView: SpringView!
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    
+    private var dataManager = DataManager()
+
+    @IBAction func animationButtonTapped(_ sender: SpringButton) {
+        
+        randomAnimationView.animation = dataManager.presets.randomElement() ?? ""
+        randomAnimationView.curve = dataManager.curves.randomElement() ?? ""
+        randomAnimationView.force = Double.random(in: 0.9...2.0)
+        randomAnimationView.duration = Double.random(in: 0.8...1.3)
+        randomAnimationView.animate()
+        
+        presetLabel.text = randomAnimationView.animation
+        curveLabel.text = randomAnimationView.curve
+        forceLabel.text = "\(round(randomAnimationView.force * 100) / 100)"
+        durationLabel.text = "\(round(randomAnimationView.duration * 100) / 100)"
     }
-
-
 }
 
